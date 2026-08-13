8f935396b54c53f21037bdbfd5cfeae27142bd3f
Piyush Jagtap <piyushjagtap541@gmail.com>
2026-08-13 11:13:26 +0530

    Update CORS for vercel frontend

--- src/main/java/com/ecommerce/security/CorsConfig.java
+++ src/main/java/com/ecommerce/security/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig
 	CorsConfigurationSource corsConfigurationSource()
 	{
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(List.of("http://localhost:3000","https://e-commerce-management-system-beta.vercel.app"));
+		configuration.setAllowedOrigins(List.of("http://localhost:3000","https://e-commerce-management-system-beta.vercel.app", "https://piyush-ecommerce.vercel.app"));
 		configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
 		configuration.setAllowedHeaders(List.of("*"));
 		configuration.setAllowCredentials(true);
